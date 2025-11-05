347f7c9ef11450676710e69f3d557ddf75b76dd4
Jesus <jesus.l.ortiz11@gmail.com>
2025-11-04 21:45:35 -0300

    feat:creacion del boton cambio de tema con mariano

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
 import AdminPage from './pages/AdminPage';
@@ -11,23 +12,39 @@ import Celulares from './pages/Celulares';
 import Televisores from './pages/Televisores';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
+
   const user = {role: "admin"}
   const userLogin = false
   return (
-    <Router>
-      <Navbar/>
-      <NavCategorias/>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/micuenta" element={!userLogin ? <MiAccount/> : <Navigate to="/" />} />
-        <Route path="/secret-dashboard" element={user.role === "admin" ? <AdminPage/> : <Navigate to="/login" />} />
-        <Route path="/computadoras" element={<Computadoras />} />
-        <Route path="/notebook" element={<Notebook />} />
-        <Route path="/celulares" element={<Celulares />} />
-        <Route path="/televisores" element={<Televisores />} />
-      </Routes>
-      <Footer/>
-    </Router>
+    <div>
+      <Router>
+        
+        <Navbar onToggleTheme={toggleDarkMode} />
+        <main className="">
+          <NavCategorias/>
+            <Routes>
+              <Route path="/" element={<HomePage/>} />
+                <Route path="/micuenta" element={!userLogin ? <MiAccount/> : <Navigate to="/" />} />
+                <Route path="/secret-dashboard" element={user.role === "admin" ? <AdminPage/> : <Navigate to="/login" />} />
+                <Route path="/computadoras" element={<Computadoras />} />
+                <Route path="/notebook" element={<Notebook />} />
+                <Route path="/celulares" element={<Celulares />} />
+                <Route path="/televisores" element={<Televisores />} />
+            </Routes>
+            </main>
+        <Footer/>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,40 @@ import NavbarBase from "./NavbarBase";
 import Dashboard from "./Dashboard";
 import AuthStatus from "./AuthStatus";
 
-const Navbar = () => {
-  const user = true
-  const cart = ['pc', 'mouse', 'teclado']
-  const isAdmin = true
+
+  function Navbar({ onToggleTheme }) {
+    const user = true;
+  const cart = ['pc', 'mouse', 'teclado'];
+  const isAdmin = true;
+
 
   return (
-    <nav className="flex justify-between px-50 py-5 bg-neutral-700">
+    <nav className="flex justify-between px-50 py-5 nav">
       <div className="flex justify-between">
-      <Link
-        className="text-2xl font-bold text-red-500 items-center space-x-2 flex"
-        to="/">
-          <p className="text-6xl">TecnoPro</p>
-      </Link>
+        <Link
+          className="text-2xl font-bold text-red-700 items-center space-x-2 flex"
+          to="/">
+          <p class="text-6xl text-red-500 font-bold">TecnoPro</p>
+        </Link>
+        
       </div>
+
+
+      <div className="flex ml-10 gap-8 items-center text-white text-lg">
+      </div>
+
       <div className="flex ml-auto ">
         <NavbarBase user={user} cart={cart} />
       </div>
+
       <div className="flex flex-col gap-8 justify-end ml-8">
         <Dashboard isAdmin={isAdmin} />
         <AuthStatus user={user} />
+      </div>
+            <div>
+        <button onClick={onToggleTheme} className="cursor-pointer flex items-center gap-2">
+          ☀️/🌙
+        </button>
       </div>
     </nav>
   );
