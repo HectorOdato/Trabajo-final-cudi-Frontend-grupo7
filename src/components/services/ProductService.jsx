06af8972e16d814d0e5847e6d158aa6c63bf35bf
HectorOdato <leoodato@gmail.com>
2025-11-16 16:53:54 -0300

    Implementé ProductService mas funciones CRUD: getAllProducts, getProductById, updateProduct y removeProduct enableProduct

--- src/components/services/ProductService.jsx
+++ src/components/services/ProductService.jsx
@@ -13,3 +13,61 @@ try {
     throw error;
 }
 };
+
+export const getProductsByCategory = async (category) => {
+try {
+    const res = await axios.get(`${API_URL}?category=${category}`);
+    return res.data;
+} catch (error) {
+    throw error;
+} };
+
+export const getAllProducts = async () => {
+try {
+    const res = await axios.get(API_URL);
+    return res.data;
+} catch (error) {
+    console.error("Error al obtener listado de productos:", error);
+    throw error;
+} };
+
+
+export const getProductbyId = async (id) => {
+try {
+    const res = await axios.get(`${API_URL}/${id}`);
+    return res.data;
+} catch (error) {
+    console.error("Error al obtener producto por ID:", error);
+    throw error;
+} };
+
+export const updateProduct = async (id, productData) => {
+try {
+    const res = await axios.put(`${API_URL}/${id}`, productData, {
+    headers: {
+        "Content-Type": "multipart/form-data",
+    },
+    });
+    return res.data;
+} catch (error) {
+    console.error("Error al actualizar producto:", error);
+    throw error;
+} };
+
+export const removeProduct = async (id) => {
+try {
+    const res = await axios.delete(`${API_URL}/${id}`);
+    return res.data;
+} catch (error) {
+    console.error("Error al eliminar producto:", error);
+    throw error;
+} };
+
+export const enableProduct = async (id) => {
+try {
+    const res = await axios.patch(`${API_URL}/${id}/enable`);
+    return res.data;
+} catch (error) {
+    console.error("Error al habilitar producto:", error);
+    throw error;
+} };
