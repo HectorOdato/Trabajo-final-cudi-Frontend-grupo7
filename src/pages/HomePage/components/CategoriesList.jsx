c8809a76d253557b6404b031800d514e300f46f2
HectorOdato <leoodato@gmail.com>
2025-11-19 20:57:16 -0300

    mejore styles css tailwind al category

--- src/pages/HomePage/components/CategoriesList.jsx
+++ src/pages/HomePage/components/CategoriesList.jsx
@@ -22,14 +22,16 @@ fetchCategories();
 
 
 return (
-  <div className="flex flex-wrap gap-6 justify-center">
-    {categories.map((category) => (
-      <CategoryItem key={category._id} category={category} />
-    )
-  )
-}
-</div>
-);
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="relative z-10 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+          {categories.map(category => (
+            <CategoryItem key={category.name} category={category} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 
