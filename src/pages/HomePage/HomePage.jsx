30621d507538a4762532c31a5fc702d634331771
HectorOdato <leoodato@gmail.com>
2025-11-24 11:15:05 -0300

    actualize URL de imagenes de carousel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,10 +2,10 @@ import CategoriesList from "../HomePage/components/CategoriesList.jsx";
 import Carousel from "../HomePage/components/Carousel.jsx"
 
 const imagescarousel = [
-  "../src/assets/images/carousel1.jpg",
-  "../src/assets/images/carousel2.png",
-  "../src/assets/images/carousel3.webp",
-  "../src/assets/images/carousel4.webp",]
+  "https://res.cloudinary.com/deijzj2mb/image/upload/v1763993483/carousel1_bdotnw.webp",
+  "https://res.cloudinary.com/deijzj2mb/image/upload/v1763993483/carousel2_ylx8rn.png",
+  "https://res.cloudinary.com/deijzj2mb/image/upload/v1763993483/carousel3_ibuxeh.webp",
+  "https://res.cloudinary.com/deijzj2mb/image/upload/v1763993483/carousel4_sviypw.webp",]
 
 const HomePage = () => {
   return ( <><Carousel images={imagescarousel}/>
