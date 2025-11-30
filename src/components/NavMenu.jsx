c882f4422c8ef59dc8f82167a8b27a156b1fc9bb
HectorOdato <leoodato@gmail.com>
2025-11-30 14:59:38 -0300

    se adapto el Nav con diseño responsive para pantallas moviles y ordenadores

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllCategories } from "./services/CategoryService";
+import { Menu, X } from "lucide-react";
 
 function NavCategorias() {
   const [categorias, setCategorias] = useState([]);
-  const [open, abrirmenu] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [menuMobile, setMenuMobile] = useState(false);
 
   useEffect(() => {
     const cargarCategorias = async () => {
@@ -21,15 +23,32 @@ function NavCategorias() {
 
   return (
     <nav className="bg-neutral-700 text-white font-bold w-full text-2xl">
-      <ul className="flex justify-center gap-8 p-4">
-    <li><Link to='/' className=" font-[Science_Gothic] text-gray-300 hover:text-red-500 transition duration-300 ease-in-out">
-    Inicio
-    </Link>
-    </li>
-        <li className="relative ">
+      <div className="flex justify-between items-center p-4 md:hidden">
+        <span className="font-[Science_Gothic] text-3xl">Menú</span>
+
+        <button
+          onClick={() => setMenuMobile(!menuMobile)}
+          className="text-white"
+        >
+          {menuMobile ? <X size={32} /> : <Menu size={32} />}
+        </button>
+      </div>
+
+      <ul className="hidden md:flex justify-center gap-8 p-4">
+        
+        <li>
+          <Link
+            to="/"
+            className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition"
+          >
+            Inicio
+          </Link>
+        </li>
+        <li className="relative">
           <button
-            onClick={() => abrirmenu(!open)}
-            className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition duration-300 ease-in-out">
+            onClick={() => setOpen(!open)}
+            className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition"
+          >
             Productos
           </button>
           {open && (
@@ -39,7 +58,8 @@ function NavCategorias() {
                   <Link
                     to={`/category/${cat._id}`}
                     className="block px-3 py-2 text-base text-gray-300 hover:text-red-400 hover:bg-neutral-700 rounded-md transition"
-                    onClick={() => abrirmenu(false)}>
+                    onClick={() => setOpen(false)}
+                  >
                     {cat.name}
                   </Link>
                 </li>
@@ -47,15 +67,80 @@ function NavCategorias() {
             </ul>
           )}
         </li>
-        <li><Link to='/faq' className=" font-[Science_Gothic] text-gray-300 hover:text-red-500 transition duration-300 ease-in-out">
-        Ayuda
-        </Link>
+        <li>
+          <Link
+            to="/faq"
+            className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition"
+          >
+            Ayuda
+          </Link>
         </li>
-        <li><Link to='/contact' className=" font-[Science_Gothic] text-gray-300 hover:text-red-500 transition duration-300 ease-in-out">
-        Contacto
-        </Link>
+        <li>
+          <Link
+            to="/contact"
+            className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition"
+          >
+            Contacto
+          </Link>
         </li>
       </ul>
+      {menuMobile && (
+        <ul className="flex flex-col gap-6 p-4 md:hidden bg-neutral-800 shadow-inner">
+          <li>
+            <Link
+              to="/"
+              className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition block"
+              onClick={() => setMenuMobile(false)}
+            >
+              Inicio
+            </Link>
+          </li>
+          <li className="">
+            <button
+              onClick={() => setOpen(!open)}
+              className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition"
+            >
+              Productos
+            </button>
+            {open && (
+              <ul className="mt-2 bg-neutral-900 rounded-lg shadow-lg p-2">
+                {categorias.map((cat) => (
+                  <li key={cat._id}>
+                    <Link
+                      to={`/category/${cat._id}`}
+                      className="block px-3 py-2 text-lg text-gray-300 hover:text-red-400 hover:bg-neutral-700 rounded-md transition"
+                      onClick={() => {
+                        setOpen(false);
+                        setMenuMobile(false);
+                      }}>
+                      {cat.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+          <li>
+            <Link
+              to="/faq"
+              className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition block"
+              onClick={() => setMenuMobile(false)}
+            >
+              Ayuda
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to="/contact"
+              className="font-[Science_Gothic] text-gray-300 hover:text-red-500 transition block"
+              onClick={() => setMenuMobile(false)}
+            >
+              Contacto
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,69 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import NavbarBase from "./NavbarBase";
 import Dashboard from "./Dashboard";
 import AuthStatus from "./AuthStatus";
-import { Lightbulb } from "lucide-react";
+import CartButton from "./CartButton";
+import { User, Lightbulb, Menu, X } from "lucide-react";
 
+function Navbar({ onToggleTheme }) {
+  const [open, setOpen] = useState(false);
 
-
-  function Navbar({ onToggleTheme }) {
-    const user = true;
-  const cart = ['pc', 'mouse', 'teclado'];
+  const user = true;
+  const cart = ["pc", "mouse", "teclado"];
   const isAdmin = true;
 
-
   return (
-    <nav className="flex justify-between px-50 py-8 nav">
-      <div className="flex justify-between">
-        <Link
-          className="text-2xl font-bold text-red-700 items-center space-x-2 flex"
-          to="/">
-          <p className="text-6xl text-red-500 font-bold">TecnoPro</p>
+    <nav className="w-full shadow-md px-4 py-6 md:px-8 lg:px-16 xl:px-32 2xl:px-48 nav text-white">
+      <div className="flex justify-between items-center">
+        <Link to="/" className="flex items-center gap-2">
+          <p className="text-4xl md:text-6xl text-red-500 font-bold">
+            TecnoPro
+          </p>
         </Link>
-
+        <div className="hidden md:flex  gap-8">
+          <Link
+            to="/micuenta"
+            className="flex gap-2 text-gray-300 hover:text-red-400 transition">
+            <User size={32} />
+          </Link>
+          <CartButton user={user} cart={cart} />
+          <AuthStatus user={user} />
+          <Dashboard isAdmin={isAdmin} />
+          <button
+            onClick={onToggleTheme}
+            className="cursor-pointer flex items-center justify-center w-12 h-12 bg-neutral-700 rounded-2xl hover:bg-neutral-600 transition shadow-md"
+          >
+            <Lightbulb className="w-6 h-6 text-yellow-300" />
+          </button>
+        </div>
+        <button
+          onClick={() => setOpen(!open)}
+          className="md:hidden text-white"
+        >
+          {open ? <X size={32} /> : <Menu size={32} />}
+        </button>
       </div>
-
-
-      <div className="flex ml-10 gap-8 items-center text-white text-lg">
-      </div>
-
-      <div className="flex ml-auto ">
-        <NavbarBase user={user} cart={cart} />
-      </div>
-
-      <div className="flex gap-8 justify-end ml-8">
-        <AuthStatus user={user} />
-        <Dashboard isAdmin={isAdmin} />
-      </div>
-            <div>
-      <button
-        onClick={onToggleTheme}
-        className="cursor-pointer flex items-center justify-center w-12 h-12 bg-neutral-700 rounded-2xl hover:bg-neutral-600 transition shadow-md">
-          <Lightbulb className="w-6 h-6 text-yellow-300 " />
-      </button>
-
-      </div>
+      {open && (
+        <div className="flex flex-col gap-6 mt-6 md:hidden">
+          <Link
+            to="/micuenta"
+            className="flex items-center gap-2 text-gray-300 hover:text-red-400 transition"
+          >
+            <User size={30} />
+          </Link>
+          <CartButton user={user} cart={cart} />
+          <AuthStatus user={user} />
+          <Dashboard isAdmin={isAdmin} />
+          <button
+            onClick={onToggleTheme}
+            className="cursor-pointer flex items-center justify-center w-12 h-12 bg-neutral-700 rounded-2xl hover:bg-neutral-600 transition shadow-md">
+            <Lightbulb className="w-6 h-6 text-yellow-300" />
+          </button>
+        </div>
+      )}
     </nav>
   );
 }
-export default Navbar
+
+export default Navbar;
+
