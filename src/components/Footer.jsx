2a1f268b399dc0df82604a5bf35cfd6c9732747c
Jesus <jesus.l.ortiz11@gmail.com>
2025-11-30 17:26:41 -0300

    feat: cambios de resolucion para celulares,tablet y notebook

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return ( <>
-    <footer id="link" className="flex flex-row justify-around bg-black text-white h-80 px-30">
-        <div className="flex flex-col items-center md:items-start justify-center md:justify-start mt-10">
+    <footer id="link" className="flex flex-col justify-around bg-black text-white md:flex-row py-10 sm:flex-col sm:items-center sm:h-auto">
+        <div className="flex flex-col items-center md:items-start">
             <p className='text-5xl text-red-500 font-bold'>TecnoPro</p>
             <p className="font-semibold text-lg mt-5 "> SUSCRIBITE</p>
             <label htmlFor="">
@@ -12,13 +12,13 @@ function Footer() {
                 <button className="bg-black  p-2  hover:bg-white hover:text-black border-1">Suscribirse</button>
             </label>
         </div>
-        <div className="space-x-4 mt-5 ml-5 gap-2 flex flex-col">
+        <div className="flex flex-col md:items-start items-center">
             <p className="font-semibold text-lg mt-3 text-red-500">SEGUINOS</p>
             <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">Facebook</a>
             <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">Twitter</a>
             <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">Instagram</a>
         </div>
-        <div className="space-y-2 mt-5 ml-5 flex flex-col">
+        <div className="flex flex-col md:items-start items-center">
             <p className="text-lg font-semibold text-red-500">INFORMACION</p>
             <Link className="hover:text-gray-400 mx-2" to="/contact">Contacto</Link>
             <Link to="/termsaconditions" className="hover:text-gray-400 mx-2">Términos y Condiciones</Link>
